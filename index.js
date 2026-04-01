502b5c11e8c6b06238fe4b6ed42f34677417ca77
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-04-01 16:39:53 +0000

    done2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+let automobiles= ['car', 'truck', 'motorcycle']
+automobiles.push('airplane', 'skateboard');
+automobiles.unshift('bike', 'helicopter');
+let automobilesCopy= `${automobiles}`;
+automobiles.pop();
+automobiles.shift();
+document.getElementById("result").innerHTML = automibles;
